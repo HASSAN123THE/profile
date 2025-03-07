335a4eee1a261696cacbc548f892421a5d7cf52b
Hassan <has0san.tk.m0hamed@gmail.com>
2025-03-07 14:12:46 +0200

    color update

--- script.js
+++ script.js
@@ -9,19 +9,28 @@ window.onload = function() {
 
 function dark() {
     let img = document.getElementById('img');
+    let email = document.getElementById('email');
+    let phone = document.getElementById('phone');
     
     if (img.src.includes('photos/moon.png')) {
-        img.src = 'photos/light.png';
+        img.src = 'photos/light mode.png';
         document.documentElement.style.setProperty('--background-color', '#111111');
-        document.documentElement.style.setProperty('--font-color', 'white');
+        document.documentElement.style.setProperty('--font-color', '#FFFFFF');
         document.documentElement.style.setProperty('--btn-color', 'white');
-        document.documentElement.style.setProperty('--primary-color', '#333333');
+        document.documentElement.style.setProperty('--bbtn-color', '#6E1AD7');
+        document.documentElement.style.setProperty('--primary-color', '#272727');
+        phone.src = 'photos/light phone.png';
+        email.src = 'photos/light email.png';
     } else {
         img.src = 'photos/moon.png';
-        document.documentElement.style.setProperty('--background-color', '#eaeaea');
+        email.src = 'photos/dark email.png';
+        phone.src = 'photos/dark phone.png';
+        document.documentElement.style.setProperty('--background-color', '#FFFFFF');
         document.documentElement.style.setProperty('--font-color', 'black');  
         document.documentElement.style.setProperty('--btn-color', 'black');  
+        document.documentElement.style.setProperty('--bbtn-color', '#28A4C2');
         document.documentElement.style.setProperty('--primary-color', '#fafafa');
+
     }
 }
 
